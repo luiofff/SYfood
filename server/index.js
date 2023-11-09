9a10cb5ed2fa376eda428eb5b84a50c49355ff40
luiofff <95875204+luiofff@users.noreply.github.com>
2023-11-09 20:34:35 +0300

    s

--- server/index.js
+++ server/index.js
@@ -6,9 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: "4c0913e8fbmshee3d5797b276796p1dabc3jsnd83be6ecfb8b",
-});
-const openai = new OpenAIApi(configuration);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
